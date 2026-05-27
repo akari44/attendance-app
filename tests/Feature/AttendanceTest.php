3029fe45e2780f1970fe1a4d398042cbaefe8265
akari.n <nishimurakarin01@gmail.com>
2026-05-28 05:50:49 +0900

    出勤、休憩機能テスト完了

--- tests/Feature/AttendanceTest.php
+++ tests/Feature/AttendanceTest.php
@@ -9,6 +9,7 @@ use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
+// ステータス確認機能
 class AttendanceTest extends TestCase
 {
     use RefreshDatabase;
@@ -37,7 +38,7 @@ class AttendanceTest extends TestCase
     public function test_status_shows_working()
     {
         $user = User::factory()->create();
-        $attendance = Attendance::create([
+        Attendance::create([
             'user_id' => $user->id,
             'date' => Carbon::today(),
             'clock_in' => '09:00:00',
@@ -58,7 +59,7 @@ class AttendanceTest extends TestCase
             'clock_in' => '09:00:00',
             'status' => '休憩中',
         ]);
-        $breaktime = BreakTime::create([
+        BreakTime::create([
             'attendance_id' => $attendance->id,
             'break_start' => Carbon::now()->format('H:i'),
         ]);
@@ -72,7 +73,7 @@ class AttendanceTest extends TestCase
     public function test_status_shows_finished()
     {
         $user = User::factory()->create();
-        $attendance = Attendance::create([
+        Attendance::create([
             'user_id' => $user->id,
             'date' => Carbon::today(),
             'clock_in' => '09:00:00',
@@ -85,6 +86,7 @@ class AttendanceTest extends TestCase
         $response->assertSee('退勤済');
     }
 
+    // 出勤機能
     public function test_clock_in_button_works()
     {
         $user = User::factory()->create();
@@ -105,7 +107,7 @@ class AttendanceTest extends TestCase
     public function test_clock_in_only_once_per_day()
     {
         $user = User::factory()->create();
-        $attendance = Attendance::create([
+        Attendance::create([
             'user_id' => $user->id,
             'date' => Carbon::today(),
             'clock_in' => '09:00:00',
@@ -120,7 +122,112 @@ class AttendanceTest extends TestCase
     public function test_clock_in_time_recorded_in_list()
     {
         $user = User::factory()->create();
+        $response = $this->actingAs($user)->get('/attendance');
+        $response->assertStatus(200);
+        $response->assertSee('勤務外');
+
+        $response = $this->actingAs($user)->post('/attendance', [
+            'action' => 'clock_in',
+        ]);
+        $response->assertRedirect('/attendance');
+
+        $response = $this->actingAs($user)->get('/attendance/list');
+        $response->assertSee(Carbon::now()->format('H:i'));
     }
+
+    // 休憩機能
+    public function test_break_start_button_work()
+    {
+        $user = User::factory()->create();
+        Attendance::create([
+            'user_id' => $user->id,
+            'date' => Carbon::today(),
+            'clock_in' => Carbon::now()->format('H:i'),
+            'status' => '出勤中',
+        ]);
+        $response = $this->actingAs($user)->get('/attendance');
+        $response->assertSee('休憩入');
+
+        $response = $this->actingAs($user)->post('/attendance', [
+            'action' => 'break_start',
+        ]);
+        $response->assertRedirect('/attendance');
+
+        $response = $this->actingAs($user)->get('/attendance');
+        $response->assertSee('休憩中');
+    }
+    public function test_user_can_break_start_any_time()
+    {
+        $user = User::factory()->create();
+        Attendance::create([
+            'user_id' => $user->id,
+            'date' => Carbon::today(),
+            'clock_in' => Carbon::now()->format('H:i:s'),
+            'status' => '出勤中',
+        ]);
+        $this->actingAs($user)->post('/attendance', ['action' => 'break_start']);
+        $this->actingAs($user)->post('/attendance', ['action' => 'break_end']);
+
+        $response = $this->actingAs($user)->get('/attendance');
+        $response->assertSee('休憩入');
+    }
+    public function test_break_end_button_work()
+    {
+        $user = User::factory()->create();
+        Attendance::create([
+            'user_id' => $user->id,
+            'date' => Carbon::today(),
+            'clock_in' => Carbon::now()->format('H:i'),
+            'status' => '出勤中',
+        ]);
+        $this->actingAs($user)->post('/attendance', ['action' => 'break_start']);
+
+        $response = $this->actingAs($user)->get('/attendance');
+        $response->assertSee('休憩戻');
+
+        $response = $this->actingAs($user)->post('/attendance', [
+            'action' => 'break_end',
+        ]);
+        $response->assertRedirect('/attendance');
+
+        $response = $this->actingAs($user)->get('/attendance');
+        $response->assertSee('出勤中');
+    }
+    public function test_user_can_break_end_any_time()
+    {
+        $user = User::factory()->create();
+        Attendance::create([
+            'user_id' => $user->id,
+            'date' => Carbon::today(),
+            'clock_in' => Carbon::now()->format('H:i:s'),
+            'status' => '出勤中',
+        ]);
+        $this->actingAs($user)->post('/attendance', ['action' => 'break_start']);
+        $this->actingAs($user)->post('/attendance', ['action' => 'break_end']);
+        $this->actingAs($user)->post('/attendance', ['action' => 'break_start']);
+
+        $response = $this->actingAs($user)->get('/attendance');
+
+        $response->assertSee('休憩戻');
+    }
+    public function test_break_times_recorded_in_list()
+    {
+        $user = User::factory()->create();
+        $attendance = Attendance::create([
+            'user_id' => $user->id,
+            'date' => Carbon::today(),
+            'clock_in' => Carbon::now()->format('H:i:s'),
+            'status' => '出勤中',
+        ]);
+        BreakTime::create([
+            'attendance_id' => $attendance->id,
+            'break_start' => Carbon::now()->subHour()->format('H:i:s'),
+            'break_end' => Carbon::now()->format('H:i:s'),
+        ]);
+        $response = $this->actingAs($user)->get('/attendance/list');
+        $response->assertSee('01:00');
+    }
+
 
 
 }
